0fb6097a389b401096a6e5409d1bbbac7bb7e4ec
Marco Koen <koenmt1@student.op.ac.nz>
2022-10-19 17:45:58 +1300

    Added date and time header

---
+++ myapp/src/lib/Attendance.svelte
@@ -0,0 +1,31 @@
+<script>
+ let currentDate = new Date().toJSON().slice(0, 10);
+ let currentTime = new Date().toLocaleTimeString();
+
+</script>
+
+<div class = "header">
+<h1>Daily Attendance Dashboard</h1>
+<p>{currentDate} | {currentTime}</p>
+</div>
+
+<div class= "attendance-containter">
+
+</div>
+
+
+
+<style>
+  .header{
+    border-radius: 15px;
+    height: 8rem;
+    width: 90%;
+    background-color: beige;
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+  }
+  
+
+</style>
